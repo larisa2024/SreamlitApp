bf1ca9b878129a893c7122874e72a9954cd6d43e
Larisa Krasheninnikova larisa.kr@yahoo.com <larisa.kr@yahoo.com>
2024-10-18 19:11:37 +0200

    first web

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 import streamlit as st
-st.write('Hello world')
+st.write('Paris, le 18 octobre 2024')
 
 st.markdown("### Happy birthday ")
-st.markdown("#### Joyeux Noel ")
 
 if st.button(label="SuperButton") :
 
     st. balloons ()
     
 
-    
-st.slider("Slider title", 0, 100, 50)       
+st.markdown("#### Joyeux Noel ")
+if  st.button("Click me"):
+    st. snow()
+    st.write("Button clicked!")
 
-st.checkbox("Checkbox title", ["Add a constant", "Add beta 1", "Add beta 2"])
+check = st.checkbox("Checkbox title", ["Add a constant", "Add beta 1", "Add beta 2"])
+radio = st.radio("Vous voulez voter?", ["Yes", "No"])
+txt = st.text_input("Votre vote")
+txt_area = st.text_area("Votre ville")
 
-
-st.radio("Radio title", ["Yes", "No"])
-
-st.text_input("Type here")
-
-st.snow()
+if  st.button("Votre nom"):
+    st.text_input("Votre nom")
