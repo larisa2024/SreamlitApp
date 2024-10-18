431edae52021d43f4e81b1723b1f503097b76cde
Larisa Krasheninnikova larisa.kr@yahoo.com <larisa.kr@yahoo.com>
2024-10-18 16:34:30 +0200

    Init Repo

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+st.write('Hello world')
+
+st.markdown("### Happy birthday ")
+st.markdown("#### Joyeux Noel ")
+
+if st.button(label="SuperButton") :
+
+    st. balloons ()
+    
+
+    
+st.slider("Slider title", 0, 100, 50)       
+
+st.checkbox("Checkbox title", ["Add a constant", "Add beta 1", "Add beta 2"])
+
+
+st.radio("Radio title", ["Yes", "No"])
+
+st.text_input("Type here")
+
+st.snow()
